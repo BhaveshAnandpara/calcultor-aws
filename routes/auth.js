049c615af1b3e41f5bcd8ee8035fabcd77b3b445
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2024-04-14 20:25:41 +0530

    added signup api

--- routes/auth.js
+++ routes/auth.js
@@ -2,24 +2,42 @@ var express = require("express");
 var app = express();
 const router = express.Router();
 const path = require("path");
-const mysql = require('mysql2');
-
+const mysql = require("mysql2");
+const { rejects } = require("assert");
 
 // Your MySQL RDS database configuration
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: 'admin',
+  user: "admin",
   password: process.env.DB_PASS,
-  database: 'calculator_db',
-  port:3306
+  database: "calculator_db",
+  port: 3306,
 });
 
 // Connect to the database
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error("Error connecting to MySQL:", err);
   }
 });
+
+//query function
+
+const query = (q) => {
+
+  return new Promise((resolve , reject)=>{
+    
+    db.query(q, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results)
+      }
+    });
+
+  })
+
+};
 
 router.get("/signup", async (req, res) => {
   res.sendFile(path.join(__dirname, "../signup.html"));
@@ -31,26 +49,17 @@ router.get("/login", async (req, res) => {
 
 router.post("/signup", async (req, res) => {
   try {
+    const { email, password } = req.body;
 
-    db.query('SELECT * FROM user', (err, results) => {
+    let userExists = await query(`select count(id) from user where email='${email}';`)
+    if(userExists != 0) res.send(200).json("user already exists")
 
-      if (err) {
-          console.log(err);
-          console.error('Error querying the database:', err);
-          res.status(500).send('Error querying the database');
-        return;
-      }
-      else{
-        console.log("ok");
-        res.json(req.results);
-      }
+    query(`insert into user values(NULL, '${email}' , '${password}');`).catch((err)=>{
+      res.status(500).json(`error occured in mysql , ${err}`)
     })
 
-    console.log("Nothing happend");
-      // Handle results
-      res.json(req.body);
+    res.send(200).json('user added successfully')
 
-    // res.json(req.body)
 
   } catch (err) {}
 });
